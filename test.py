711c7da88a93931440e772aef2e2d794a78cc850
Beiwen Liu <bliu85@gatech.edu>
2017-01-19 12:58:49 -0500

    Add histogram for duration look away and minimum threshold

--- test.py
+++ test.py
@@ -54,6 +54,14 @@ def main():
     medianX = median(xValues)
     
     threshold = raw_input("State your offset from average\n")
+    durationthreshold = raw_input("State your duration threshold\n")
+    if durationthreshold == "":
+        durationthreshold = 0
+    else:
+        durationthreshold = float(durationthreshold)
+        
+    #.066 conservative
+    #.2 liberal
     
     totalTime = timeValues[-1] - timeValues[0]
     distractedTime = calculateTotalDistractionTime(medianX,threshold,xValues,timeValues)
@@ -71,12 +79,13 @@ def main():
             newTimeValues.append(timeValues[i])
             newxValues.append(xValues[i])
     
-    xStart,yStart,xEnd,yEnd,averageDistance = calculations(medianX,threshold,newxValues,newTimeValues) #Finds start-end for +x
-    xStart1,yStart1,xEnd1,yEnd1,averageDistance1 = calculations2(medianX,threshold,newxValues,newTimeValues) #Finds start-end for -x
+    xStart,yStart,xEnd,yEnd,averageDistance,durations = calculations(medianX,threshold,newxValues,newTimeValues,durationthreshold) #Finds start-end for +x
+    xStart1,yStart1,xEnd1,yEnd1,averageDistance1,durations2 = calculations2(medianX,threshold,newxValues,newTimeValues,durationthreshold) #Finds start-end for -x
     graphX(xStart,yStart,xEnd,yEnd,xStart1,yStart1,xEnd1,yEnd1,timeValues,xValues, medianX) #plots everything
     
-    #histogram(xValues) # To enable histogram, uncomment here
-    
+    durations.extend(durations2) #combine both durations from top and bottom
+    #histogram(xValues) # To enable histogram for distance look away, uncomment here
+    #histogramDurations(durations) # To enable histogram for duration of look aways, uncomment here
     total = findDuration(xStart,xEnd)
     print round(total/totalTime*100,2)
     print "distraction: " + str(round(distractedTime/totalTime*100,2)) + "%"
@@ -110,7 +119,7 @@ def saveToExcel(median,mean,startTime,endTime,averageXDistance,dataDistractionPe
     wb.save(new_file)
 
 #This method will find the start and end points
-def calculations(averageX,threshold,xValues,timeValues):
+def calculations(averageX,threshold,xValues,timeValues,durationthreshold):
     threshold = float(threshold)
     
     found = False
@@ -130,6 +139,9 @@ def calculations(averageX,threshold,xValues,timeValues):
     averageDistance = []
 
     tempDistance = []
+
+    counter = 0
+    durations = []
     
     
     for x in range(2,len(xValues)):
@@ -148,17 +160,22 @@ def calculations(averageX,threshold,xValues,timeValues):
             endX = slopeX(currentY,previousY,currentX,previousX,threshold + averageX)
             endY = averageX + threshold
             found = False
-            startXY.append([startX,startY]) #column 0 is the time and column 1 is the "x" distance
-            endXY.append([endX,endY])
-            averageDistance.append(sum(tempDistance) / len(tempDistance))
+            if endX-startX > durationthreshold:
+                startXY.append([startX,startY]) #column 0 is the time and column 1 is the "x" distance
+                endXY.append([endX,endY])
+                durations.append(endX-startX)
+                counter = counter + 1
+                averageDistance.append(sum(tempDistance) / len(tempDistance))
+                
             tempDistance = []
             
         previousY = currentY
         previousX = currentX
+    print "occurrences", counter
 
-    return [row[0] for row in startXY], [row[1] for row in startXY], [row[0] for row in endXY], [row[1] for row in endXY], averageDistance
+    return [row[0] for row in startXY], [row[1] for row in startXY], [row[0] for row in endXY], [row[1] for row in endXY], averageDistance, durations
         
-def calculations2(averageX,threshold,xValues,timeValues):
+def calculations2(averageX,threshold,xValues,timeValues,durationthreshold):
     threshold = float(threshold)
     
     found = False
@@ -179,6 +196,7 @@ def calculations2(averageX,threshold,xValues,timeValues):
 
     tempDistance = []
     
+    durations = []
     
     for x in range(2,len(xValues)):
         currentY = xValues[x] # X Distance Value
@@ -196,15 +214,17 @@ def calculations2(averageX,threshold,xValues,timeValues):
             endX = slopeX(currentY,previousY,currentX,previousX,averageX - threshold)
             endY = averageX - threshold
             found = False
-            startXY.append([startX,startY]) #column 0 is the time and column 1 is the "x" distance
-            endXY.append([endX,endY])
-            averageDistance.append(sum(tempDistance) / len(tempDistance))
+            if endX-startX > durationthreshold:
+                startXY.append([startX,startY]) #column 0 is the time and column 1 is the "x" distance
+                endXY.append([endX,endY])
+                durations.append(endX-startX)
+                averageDistance.append(sum(tempDistance) / len(tempDistance))
             tempDistance = []
             
         previousY = currentY
         previousX = currentX
 
-    return [row[0] for row in startXY], [row[1] for row in startXY], [row[0] for row in endXY], [row[1] for row in endXY], averageDistance
+    return [row[0] for row in startXY], [row[1] for row in startXY], [row[0] for row in endXY], [row[1] for row in endXY], averageDistance, durations
 
 def calculateTotalDistractionTime(averageX,threshold,xValues,timeValues):
     distractedTime = 0
@@ -297,6 +317,35 @@ def histogram(sa):
     ax = hist.plot(kind='bar', width=1, alpha=0.5, align='center')
     ax.set_title('Uniform Bin Distribution of Distance Look Away')
     ax.set_xlabel('Distance Look Away')
+    
+#Histogram for distribution of durations 
+def histogramDurations(sa):
+    
+    sa = [int(round(x)) for x in sa]
+    minimum = min(sa)
+    maximum = max(sa)
+    binSize = (maximum-minimum) / 100
+    print "max",maximum,"min",minimum, "bin",binSize
+    
+    # Get histogram of random data
+    bins = []
+    x = minimum
+    while x <= maximum:
+        bins.append(x)
+        x += binSize
+        
+    y, x = np.histogram(sa, bins=bins)
+    
+    # Correct bin placement
+    x = x[1:]
+    
+    # Turn into pandas Series
+    hist = pd.Series(y, x)
+    
+    # Plot
+    ax = hist.plot(kind='bar', width=1, alpha=0.5, align='center')
+    ax.set_title('Uniform Bin Distribution of Look Durations')
+    ax.set_xlabel('Duration of Look Away')
 
 
     
